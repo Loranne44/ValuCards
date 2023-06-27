367e2fea17cbaa395891b51e7f2d6979e5eac3ae
Loranne44 <91254324+Loranne44@users.noreply.github.com>
2023-06-27 10:55:34 +0200

    Creating the visual for the authentication page

--- ValuCards/Controller/ViewController.swift
+++ ValuCards/Controller/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SignInButton: UIButton!
+    @IBOutlet weak var CreateNewAccountButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Sign In Button
+        SignInButton.layer.cornerRadius = 15
+        
+        //Create New Account Button
+        CreateNewAccountButton.layer.cornerRadius = 15
     }
 
 
